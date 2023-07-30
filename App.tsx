611be386a1c75b9adec7504935068f07eafeb5e2
Igor Medeiros <igormedeirosandrade@gmail.com>
2023-07-29 21:26:23 -0300

    Primeiro commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import {Text, StyleSheet, View, ScrollView} from 'react-native';
+import React, {Component} from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <ScrollView>
+        <View>
+          <Text style={styles.space}>Test</Text>
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  space: {
+    padding: 10,
+    color: '#000',
+  },
+});
